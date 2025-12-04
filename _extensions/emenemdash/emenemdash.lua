d0352dfb8df25eedb8f4d1998b5a1e0ed185a4d0
Michael Lydeamore <michael.lydeamore@monash.edu>
2025-12-04 15:41:48 +0800

    Add more dashes!

--- _extensions/emenemdash/emenemdash.lua
+++ _extensions/emenemdash/emenemdash.lua
@@ -7,6 +7,22 @@ local input_dashes = {
   "−",   -- minus sign
   "‒",   -- figure dash
   "‐",   -- Unicode hyphen
+  "\u{002D}", -- hyphen-minus
+  "\u{2010}", -- hyphen
+  "\u{2212}", -- minus sign
+  "\u{2011}", -- non-breaking hyphen
+  "\u{2012}", -- figure dash
+  "\u{2013}", -- en dash
+  "\u{2014}", -- em dash
+  "\u{2015}", -- horizontal bar
+  "\u{2E40}", -- double hyphen
+  "\u{2E17}", -- double oblique hyphen
+  "\u{FE58}", -- small em dash
+  "\u{FE63}", -- small hyphen-minus
+  "\u{FF0D}", -- fullwidth hyphen-minus
+  "\u{1163}", -- Hangul jungseong eu
+  "\u{2E3A}", -- two-em dash
+  "\u{2E3B}", -- three-em dash
 }
 
 local function pick_random(tbl)
