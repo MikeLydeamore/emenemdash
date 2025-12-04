99593d88708a62759bf2793b7591d1b0ce0e2593
Michael Lydeamore <michael.lydeamore@monash.edu>
2025-12-04 15:35:06 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Emenemdash Extension For Quarto
+
+Having arguments about what kind of dash to use? This extension solves it.
+
+## Installing
+
+```bash
+quarto add MikeLydeamore/emenemdash
+```
+
+This will install the extension under the `_extensions` subdirectory.
+If you're using version control, you will want to check in this directory.
+
+## Using
+
+Yeah you've not gotta do anything.
+
+## Example
+
+Here is the source code for a minimal example: [example.qmd](example.qmd).
+
---
+++ _extensions/emenemdash/emenemdash.lua
@@ -0,0 +1,36 @@
+-- emenemdash.lua
+
+local replacements = { "–", "—", "−" }
+
+local input_dashes = {
+  "-",   -- ASCII hyphen-minus
+  "–",   -- en dash
+  "—",   -- em dash
+  "−",   -- minus sign
+  "‒",   -- figure dash
+  "‐",   -- Unicode hyphen
+}
+
+local function pick_random(tbl)
+  return tbl[math.random(#tbl)]
+end
+
+function Meta(meta)
+  local seed = os.time()
+  math.randomseed(seed)
+  return meta
+end
+
+function Str(el)
+  local s = el.text
+
+  for _, dash in ipairs(input_dashes) do
+    local pattern = dash:gsub("(%p)", "%%%1")
+    s = s:gsub(pattern, function()
+      return pick_random(replacements)
+    end)
+  end
+
+  el.text = s
+  return el
+end
